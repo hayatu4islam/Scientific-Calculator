81a9c1356e49497f42eb0e2d90fac68cbabda89e
Hayat <hayatu4islam@yahoo.com>
2023-03-26 19:05:17 +0100

    Keypads added

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -5,11 +5,58 @@ function App() {
     <div className="App">
       <div className="Calc-App">
         <div className="Calc-App-Display">
-          <h1 className="display-text">10</h1>
+          <h1 className="display-text">100</h1>
         </div>
-        {/* <div className="Calc-App-Keypad"> */}
-          {/* <button type="button">C</button> */}
-        {/* </div> */}
+        <div className="Calc-App-Keypads">
+          <button className="keypad" type="button">
+            C
+          </button>
+          <button className="keypad" type="button">
+            +/-
+          </button>
+          <button className="keypad" type="button">
+            %
+          </button>
+          <button className="keypad" type="button">
+            ÷
+          </button>
+          <button className="keypad" type="button">
+            7
+          </button>
+          <button className="keypad" type="button">
+            8
+          </button>
+          <button className="keypad" type="button">
+            9
+          </button>
+          <button className="keypad" type="button">
+            x
+          </button>
+          <button className="keypad" type="button">
+            4
+          </button>
+          <button className="keypad" type="button">
+            5
+          </button>
+          <button className="keypad" type="button">
+            6
+          </button>
+          <button className="keypad" type="button">
+            -
+          </button>
+          <button className="keypad" type="button">
+            1
+          </button>
+          <button className="keypad" type="button">
+            2
+          </button>
+          <button className="keypad" type="button">
+            3
+          </button>
+          <button className="keypad" type="button">
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
